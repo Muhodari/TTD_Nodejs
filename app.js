4a0c3db71e0c5eed9b959bec1cba10ea8ec692b6
ndondoRugaba <sagemuho@gmail.com>
2022-01-15 18:34:40 +0200

    initiating

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require("express");
 
 const app = express();
 
+
+app.get("",(req,res)=>{
+    res.status(200).json({
+        message:"welcome to our App"
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("app is running")
 })
+
