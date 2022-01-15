270de55ced819fdc804350d44b56e4346a2a7605
ndondoRugaba <sagemuho@gmail.com>
2022-01-15 20:39:49 +0200

    config prettier

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const express = require("express");
 
 const app = express();
 
-
-app.get("",(req,res)=>{
-    res.status(200).json({
-        message:"welcome to our App"
-    })
-})
-
 app.listen(3000, ()=>{
-    console.log("app is running")
+    console.log("app.is running")
 })
 
