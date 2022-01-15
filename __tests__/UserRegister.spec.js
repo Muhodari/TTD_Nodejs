83292264fc2e7f128d092ffa711804c2e9d86678
ndondoRugaba <sagemuho@gmail.com>
2022-01-16 00:27:30 +0200

    done with user registration tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,13 +1,17 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app');
 
-it("returns 200 OK when signup request is valid", (done) => {
+it('returns 200 OK when signup request is valid', (done) => {
   request(app)
-    .post("/api/1.0/users")
+    .post('/api/1.0/users')
     .send({
-      username: "user1",
-      email: "user1@mail.com",
-      password: "P4ssword",
+      username: 'user1',
+      email: 'user1@mail.com',
+      password: 'P4ssword',
     })
-    .expect(200, done);
+    .then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
+  // .expect(200, done);
 });
